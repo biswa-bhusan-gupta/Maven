fe65e5af948919e5a521b86d1bea4ee03e8f476c
Biswa Bhusan Gupta <66106600+biswa-bhusan-gupta@users.noreply.github.com>
2022-06-10 17:03:50 +0530

    Add files via upload

---
+++ src/main/java/com/sample/com/sample/App.java
@@ -0,0 +1,15 @@
+package com.sample.com.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String data = null;
+        System.out.println( "Hello World!" + data.toUpperCase() );
+        System.out.println(10/0);
+    }
+}
